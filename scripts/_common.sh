1b5d017b4c3270450adcbc112f2b7f5e9eac65b6
anmol26s <github@datamol.org>
2018-05-27 15:32:55 +0530

    Fixed install and remove script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,45 +6,7 @@
 
 # Create a dedicated nginx config
 #
-# usage: ynh_add_nginx_config
-ynh_add_nginx_config () {
-	finalnginxconf="/etc/nginx/conf.d/$domain.d/$app.conf"
-	ynh_backup_if_checksum_is_different "$finalnginxconf"
-	sudo cp ../conf/nginx.conf "$finalnginxconf"
 
-	# To avoid a break by set -u, use a void substitution ${var:-}. If the variable is not set, it's simply set with an empty variable.
-	# Substitute in a nginx config file only if the variable is not empty
-	if test -n "${path_url:-}"; then
-		ynh_replace_string "__PATH__" "$path_url" "$finalnginxconf"
-	fi
-	if test -n "${domain:-}"; then
-		ynh_replace_string "__DOMAIN__" "$domain" "$finalnginxconf"
-	fi
-	if test -n "${port:-}"; then
-		ynh_replace_string "__PORT__" "$port" "$finalnginxconf"
-	fi
-	if test -n "${app:-}"; then
-		ynh_replace_string "__NAME__" "$app" "$finalnginxconf"
-	fi
-	if test -n "${final_path:-}"; then
-		ynh_replace_string "__FINALPATH__" "$final_path" "$finalnginxconf"
-	fi
-	ynh_store_file_checksum "$finalnginxconf"
-
-	sudo systemctl reload nginx
-}
-
-# Remove the dedicated nginx config
-#
-# usage: ynh_remove_nginx_config
-ynh_remove_nginx_config () {
-	ynh_secure_remove "/etc/nginx/conf.d/$domain.d/$app.conf"
-	sudo systemctl reload nginx
-}
-
-# Create a dedicated php-fpm config
-#
-# usage: ynh_add_fpm_config
 ynh_add_fpm_config () {
 	finalphpconf="/etc/php5/fpm/pool.d/$app.conf"
 	ynh_backup_if_checksum_is_different "$finalphpconf"
@@ -76,40 +38,7 @@ ynh_remove_fpm_config () {
 	sudo systemctl reload php5-fpm
 }
 
-# Create a dedicated systemd config
-#
-# usage: ynh_add_systemd_config
-ynh_add_systemd_config () {
-	finalsystemdconf="/etc/systemd/system/$app.service"
-	ynh_backup_if_checksum_is_different "$finalsystemdconf"
-	sudo cp ../conf/systemd.service "$finalsystemdconf"
 
-	# To avoid a break by set -u, use a void substitution ${var:-}. If the variable is not set, it's simply set with an empty variable.
-	# Substitute in a nginx config file only if the variable is not empty
-	if test -n "${final_path:-}"; then
-		ynh_replace_string "__FINALPATH__" "$final_path" "$finalsystemdconf"
-	fi
-	if test -n "${app:-}"; then
-		ynh_replace_string "__APP__" "$app" "$finalsystemdconf"
-	fi
-	ynh_store_file_checksum "$finalsystemdconf"
-
-	sudo chown root: "$finalsystemdconf"
-	sudo systemctl enable $app
-	sudo systemctl daemon-reload
-}
-
-# Remove the dedicated systemd config
-#
-# usage: ynh_remove_systemd_config
-ynh_remove_systemd_config () {
-	finalsystemdconf="/etc/systemd/system/$app.service"
-	if [ -e "$finalsystemdconf" ]; then
-		sudo systemctl stop $app
-		sudo systemctl disable $app
-		ynh_secure_remove "$finalsystemdconf"
-	fi
-}
 
 # Execute a command as another user
 # usage: exec_as USER COMMAND [ARG ...]
@@ -180,76 +109,6 @@ sudo_path () {
 # Utilisé pour démarrer un service ou un script qui utilise node ou npm
 # Dans ce cas, c'est $PATH qui contient le chemin de la version de node. Il doit être propagé sur les autres shell si nécessaire.
 
-n_install_dir="/opt/node_n"
-ynh_use_nodejs () {
-	nodejs_version=$(ynh_app_setting_get $app nodejs_version)
-
-	load_n_path="[[ :$PATH: == *\":$n_install_dir/bin:\"* ]] || PATH+=\":$n_install_dir/bin\""
-
-	nodejs_use_version="n $nodejs_version"
-
-	# "Load" a version of node
-	eval $load_n_path; $nodejs_use_version
-	eval $load_n_path; sudo env "PATH=$PATH" $nodejs_use_version
-
-	# Get the absolute path of this version of node
-	nodejs_path="$(n bin $nodejs_version)"
-
-	# Make an alias for node use
-	ynh_node_exec="eval $load_n_path; n use $nodejs_version"
-	sudo_ynh_node_exec="eval $load_n_path; sudo env \"PATH=$PATH\" n use $nodejs_version"
-}
-
-ynh_install_nodejs () {
-	# Use n, https://github.com/tj/n to manage the nodejs versions
-	local nodejs_version="$1"
-	local n_install_script="https://git.io/n-install"
-
-	# Create $n_install_dir
-	sudo mkdir -p "$n_install_dir"
-
-	# Load n path in PATH
-	PATH+=":$n_install_dir/bin"
-
-	# If n is not previously setup, install it
-	n --version > /dev/null 2>&1 || \
-	( echo "Installation of N - Node.js version management" >&2; \
-	curl -sL $n_install_script | sudo N_PREFIX="$n_install_dir" bash -s -- -y $nodejs_version )
-
-	# Install the requested version of nodejs (except for the first installation of n, which installed the requested version of node.)
-	sudo env "PATH=$PATH" n $nodejs_version
-
-	# Use the real installed version. Sometimes slightly different
-	nodejs_version=$(node --version | cut -c2-)
-
-	# Store the ID of this app and the version of node requested for it
-	echo "$YNH_APP_ID:$nodejs_version" | sudo tee --append "$n_install_dir/ynh_app_version"
-
-	# Store nodejs_version into the config of this app
-	ynh_app_setting_set $app nodejs_version $nodejs_version
-
-	ynh_use_nodejs
-}
-
-ynh_remove_nodejs () {
-	ynh_use_nodejs
-
-	# Remove the line for this app
-	sudo sed --in-place "/$YNH_APP_ID:$nodejs_version/d" "$n_install_dir/ynh_app_version"
-
-	# If none another app uses this version of nodejs, remove it.
-	if ! grep --quiet "$nodejs_version" "$n_install_dir/ynh_app_version"
-	then
-		n rm $nodejs_version
-	fi
-
-	# If none another app uses n, remove n
-	if [ ! -s "$n_install_dir/ynh_app_version" ]
-	then
-		ynh_secure_remove "$n_install_dir"
-		sudo sed --in-place "/N_PREFIX/d" /root/.bashrc
-	fi
-}
 
 #
 # PHP7 helpers
